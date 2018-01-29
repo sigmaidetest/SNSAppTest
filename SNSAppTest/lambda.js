aaf58f99869c0bbe774b4e2633016cb26d421c60
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-29 11:55:08 +0530

    Added remove endpoint code

--- SNSAppTest/lambda.js
+++ SNSAppTest/lambda.js
@@ -15,18 +15,29 @@ exports.handler = function (event, context, callback) {
 	// 		console.log(err);
 	// 	});
 
-	sns.publish({
-		Message: 'My message',
-		MessageAttributes: {},
-		MessageStructure: 'String',
-		TargetArn: 'arn:aws:sns:us-east-1:480964559519:endpoint/GCM/Test-GCM-App/15a983de-02ec-34ca-9740-9a6ff727ab98'
+	sns.deleteEndpoint({
+		EndpointArn: 'arn:aws:sns:us-east-1:480964559519:endpoint/GCM/Test-GCM-App/15a983de-02ec-34ca-9740-9a6ff727ab98'
 	}).promise()
 		.then(data => {
-			console.log(data);
+			// your code goes here
 		})
 		.catch(err => {
-			console.log(err);
+			// error handling goes here
 		});
+
+
+	// sns.publish({
+	// 	Message: 'My message',
+	// 	MessageAttributes: {},
+	// 	MessageStructure: 'String',
+	// 	TargetArn: 'arn:aws:sns:us-east-1:480964559519:endpoint/GCM/Test-GCM-App/15a983de-02ec-34ca-9740-9a6ff727ab98'
+	// }).promise()
+	// 	.then(data => {
+	// 		console.log(data);
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err);
+	// 	});
 
 
 
