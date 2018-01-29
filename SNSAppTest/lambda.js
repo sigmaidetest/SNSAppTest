cbd7ed60c03a1339895981aa974cb4a2354eb19f
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-29 11:39:33 +0530

    Added send msg to endpoint code

--- SNSAppTest/lambda.js
+++ SNSAppTest/lambda.js
@@ -2,11 +2,24 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
-	sns.createPlatformEndpoint({
-		PlatformApplicationArn: 'arn:aws:sns:us-east-1:480964559519:app/GCM/Test-GCM-App',
-		Token: '12345',
-		CustomUserData: 'My Data',
-		Attributes: {},
+	// sns.createPlatformEndpoint({
+	// 	PlatformApplicationArn: 'arn:aws:sns:us-east-1:480964559519:app/GCM/Test-GCM-App',
+	// 	Token: '12345',
+	// 	CustomUserData: 'My Data',
+	// 	Attributes: {},
+	// }).promise()
+	// 	.then(data => {
+	// 		console.log(data);
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err);
+	// 	});
+
+	sns.publish({
+		Message: 'My message',
+		MessageAttributes: {},
+		MessageStructure: 'String',
+		TargetArn: 'arn:aws:sns:us-east-1:480964559519:endpoint/GCM/Test-GCM-App/15a983de-02ec-34ca-9740-9a6ff727ab98'
 	}).promise()
 		.then(data => {
 			console.log(data);
@@ -14,6 +27,7 @@ exports.handler = function (event, context, callback) {
 		.catch(err => {
 			console.log(err);
 		});
+
 
 
 	callback(null, 'Successfully executed');
